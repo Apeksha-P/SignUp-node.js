8111bb06b4daacfb712d5faac8562c8cc21a1cc2
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-10-11 17:51:10 +0530

    Content update

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const path =  require("path")
+const hbs = require("hbs")
+const collection = require("./mongodb")
+
+const remplatePath = path.join(__dirname,'../templates')
+
+app.use(express.json())
+app.set("view engine","hbs")
+app.set("views",remplatePath)
+app.use(express.urlencoded({
+    extends:false
+}))
+
+app.get("/",(req,res)=>{
+    res.render("login")
+})
+app.get("/signup",(req,res)=>{
+    res.render("signup")
+})
+
+app.post("/login",async(req,res)=>{
+    res.render("signup")
+})
+
+app.post("/signup",async(req,res)=>{
+    const data={
+        name:req.body.name,
+        password:req.body.password
+    }
+    await collection.insertMany([data])
+    res.render("home")
+})
+
+app.listen(3000,()=>{
+    console.log("port connect");
+})
