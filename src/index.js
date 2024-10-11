7ccde51411f5438f3cca312c37d40935f4c8e94a
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-10-11 20:28:50 +0530

    Create sign page using node.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
-const express = require("express")
-const app = express()
-const path =  require("path")
-const hbs = require("hbs")
-const collection = require("./mongodb")
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
 
-const remplatePath = path.join(__dirname,'../templates')
+const remplatePath = path.join(__dirname, '../templates');
 
-app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",remplatePath)
-app.use(express.urlencoded({
-    extends:false
-}))
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", remplatePath);
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>{
-    res.render("login")
-})
-app.get("/signup",(req,res)=>{
-    res.render("signup")
-})
+app.get("/", (req, res) => {
+    res.render("login");
+});
 
-app.post("/login",async(req,res)=>{
-    res.render("signup")
-})
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
 
-app.post("/signup",async(req,res)=>{
-    const data={
-        name:req.body.name,
-        password:req.body.password
+app.post("/signup", async (req, res) => {
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    };
+    await collection.insertMany([data]);
+    res.render("home");
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.name });
+        if (check && check.password === req.body.password) {
+            res.render("home");
+        } else {
+            res.send("Wrong Password");
+        }
+    } catch (error) {
+        res.send("Wrong Details");
     }
-    await collection.insertMany([data])
-    res.render("home")
-})
+});
 
-app.listen(3000,()=>{
-    console.log("port connect");
-})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
